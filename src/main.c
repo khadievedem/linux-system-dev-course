9e602553b1e8d5c95f904f2da89ebfdcfd8b1ccb
khadievedem <khadiev.edem@gmail.com>
2023-04-11 12:32:27 +0300

    use init_library instead of including hello.h

--- src/main.c
+++ src/main.c
@@ -1,6 +1,27 @@
-#include "hello.h"
+/* #include "hello.h" */
+#include <dlfcn.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+
+void (*print_msg)(const char *);
+
+bool init_library() {
+  void *hdl = dlopen("lib/libhello.so", RTLD_LAZY);
+  if (NULL == hdl) {
+    return false;
+  }
+  print_msg = (void (*)(const char *))dlsym(hdl, "print_msg");
+  if (NULL == print_msg) {
+    return false;
+  }
+  return true;
+}
 
 int main() {
-  print_msg("hello world!");
+  if (init_library())
+    print_msg("hello world!");
+  else
+    printf("Library was not found!\n");
   return 0;
 }
