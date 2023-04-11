f789937b569ff1e992f8f403403a1999ff95e72a
khadievedem <khadiev.edem@gmail.com>
2023-04-11 12:53:21 +0300

    fix 1st solution

--- src/tasks/first/solution.c
+++ src/tasks/first/solution.c
@@ -1,11 +1,10 @@
-#include "solution.h"
 #include <stddef.h>
-#include <stdio.h>
+#include "solution.h"
 
 int stringStat(const char *string, size_t multiplier, int *count) {
   int res = 0;
   for (; *string; res++, string++)
     ;
-  count++;
+  ++*count;
   return res * multiplier;
 }
