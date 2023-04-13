9fe7a7a93a8d014c6da5880244185b990442894a
khadievedem <khadiev.edem@gmail.com>
2023-04-13 12:41:42 +0300

    use getpid() and /proc/pid/stat to fid PPID

--- src/tasks/third/solution.c
+++ src/tasks/third/solution.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int main() {
+  char *f_p = malloc(100);
+  sprintf(f_p, "/proc/%d/stat", getpid());
+  FILE *stat_file = fopen(f_p, "r");
 
-  printf("%d\n", getppid());
+  int ppid;
+  fscanf(stat_file, "%*d %*s %*c %d", &ppid);
 
+  printf("%d\n", ppid);
+
+  free(f_p);
   return 0;
 }
